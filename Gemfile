a1848cdba0f53e593e40e34a9bf98d4168d19fab
Ganesh Sagare <ganesh@joshsoftware.com>
2017-04-21 12:50:37 +0530

    [Added] haml-rails gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 # HTML Abstraction Markup Language - A Markup Haiku
 gem 'haml'
+gem 'haml-rails'
 # bootstrap
 gem 'bootstrap-sass', '~> 3.3.6'
 
--- app/assets/javascripts/blogs.js.ts
+++ app/assets/javascripts/blogs.js.ts
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+class Hello{
+  say(){
+    console.log("Hello");
+  }
+}
+
+var hello = new Hello();
+hello.say();
